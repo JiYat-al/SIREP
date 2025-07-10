f9ff9fb8fcd5985beadc2e9be92fcebcecfcdc24
IsaacCruz900STRA <c.isaac900stra@gmail.com>
2025-07-10 14:00:58 -0600

    quinto commit

--- src/Controlardor/Hello_2.java
+++ src/Controlardor/Hello_2.java
@@ -2,6 +2,8 @@ package Controlardor;
 
 public class Hello_2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int x = 789293;
+        System.out.println("Hello World" + x);
     }
 }
+  
