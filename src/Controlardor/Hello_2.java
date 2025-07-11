3cd53823e1615c705e247cdc2a0777abee8ee359
nierikitahermoxxxa <nierikaginer@gmail.com>
2025-07-10 18:30:50 -0600

    Prueba

--- src/Controlardor/Hello_2.java
+++ src/Controlardor/Hello_2.java
@@ -2,6 +2,7 @@ package Controlardor;
 
 public class Hello_2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
+
     }
 }
